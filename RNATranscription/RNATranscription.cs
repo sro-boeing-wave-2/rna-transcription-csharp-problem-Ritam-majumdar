48c00bdd806f4159d7da8d0cdd9dce12ef9fce7b
Ritam Majjumdar <majumdar_ritam@yahoo.com>
2018-07-19 11:16:03 +0530

    updated RNATranscription.cs

--- RNATranscription/RNATranscription.cs
+++ RNATranscription/RNATranscription.cs
@@ -6,6 +6,24 @@ namespace RNATranscriptionModule
     {
         public static string ToRna(string nucleotide)
         {
+            string newString = "";
+            for(int i=0;i<nucleotide.Length;i++)
+            {
+                
+                switch (nucleotide[i])
+                {
+                    case 'G': newString+='C';
+                        break;
+                    case 'C': newString += 'G';
+                        break;
+                    case 'T': newString += 'A';
+                        break;
+                    case 'A': newString += 'U';
+                        break;
+                }
+
+            }
+            return newString;
             throw new NotImplementedException("You need to implement this function.");
         }
     }
